a145a10dec67bddf315ff432748819ddcfc65bbc
todokr <shunsuke.tadokoro@bizreach.co.jp>
2018-09-01 02:40:38 +0900

    add slides

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,32 +1,18 @@
 package controllers
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
 import com.google.inject.Inject
-import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.mvc.{AbstractController, ControllerComponents}
-import slick.jdbc.JdbcProfile
 
-import models.Tables
-import models.Tables._
-import Tables.profile.api._
 
 class UserController @Inject()(
-  controllerComponents: ControllerComponents,
-  val dbConfigProvider: DatabaseConfigProvider
-) extends AbstractController(controllerComponents) with HasDatabaseConfigProvider[JdbcProfile] {
+  controllerComponents: ControllerComponents
+) extends AbstractController(controllerComponents){
 
   // 一覧画面の表示
   def list(authority: Option[String]) = Action { implicit request =>
 
-    val dbAction = authority.map { a =>
-      User.filter(_.authority === a.bind).sortBy(_.userId).result
-    }.getOrElse(User.sortBy(_.userId).result)
 
-    val result = Await.result(db.run(dbAction), Duration.Inf)
-
-    Ok(views.html.user.list(result))
+    Ok(views.html.user.list(Nil))
   }
 
   // 編集画面の表示
--- build.sbt
+++ build.sbt
@@ -10,8 +10,9 @@ scalaVersion := "2.12.6"
 libraryDependencies ++= Seq(
   guice,
   "mysql" % "mysql-connector-java" % "6.0.6",
-  "com.typesafe.play" %% "play-slick" % "3.0.1",
-  "com.typesafe.slick" %% "slick-codegen" % "3.2.0",
+  "org.scalikejdbc" %% "scalikejdbc" % "3.2.2",
+  "org.scalikejdbc" %% "scalikejdbc-config" % "3.2.2",
+  "org.scalikejdbc" %% "scalikejdbc-play-initializer" % "2.6.0-scalikejdbc-3.2",
   "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
 )
 
